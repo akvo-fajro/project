75a0805a77d95218f17a5e036e19ccdac6191a92
akvo-fajro <96491243+akvo-fajro@users.noreply.github.com>
2022-03-28 21:28:42 +0800

    Delete SchoolLunchWeb directory

--- SchoolLunchWeb/users_additional_information/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-# Create your models here.
-class UserAdditionalInformation(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    sit_number = models.DecimalField(max_digits=2,decimal_places=0)
-    money_to_pay = models.DecimalField(max_digits=10,decimal_places=0,default=0)
-    money_pay_back = models.DecimalField(max_digits=10,decimal_places=0,default=0)
